5e95df44a06baa148c2660126e41696664028082
tand826 <takumi.ando826@gmail.com>
2020-03-21 00:09:10 +0900

    barely active

--- popup.js
+++ popup.js
@@ -1,40 +1,73 @@
+navigator.webkitGetUserMedia(
+  {
+    audio: true
+  },
+  function(stream) {
+    alert("successed");
+    // Now you know that you have audio permission. Do whatever you want...
+  },
+  function() {
+    alert("failed");
+    // Aw. No permission (or no microphone available).
+  }
+);
+
 var recognizing;
+stop();
 var recognition = new webkitSpeechRecognition();
 recognition.continuous = true;
-reset();
-recognition.onend = reset;
-recognition.lang = "ja";
+recognition.interimResults = true;
+recognition.onend = stop();
+recognition.lang = "ja-JP";
+recognition.onstart = e => showMessage("start");
+//recognition.onaudiostart = e => showMessage("audio start");
+//recognition.onsoundstart = e => showMessage("sound start");
+recognition.onspeechstart = e => showMessage("speech start");
+recognition.onspeechend = e => showMessage("speech end");
+//recognition.onsoundend = e => showMessage("sound end");
+//recognition.onaudioend = e => showMessage("audio end");
+recognition.onend = e => showMessage("end");
+
+recognition.onerror = e => showMessage("error");
+recognition.onnomatch = e => showMessage("nomatch");
+
 recognition.onresult = function(event) {
+  console.log(event);
   for (var i = event.resultIndex; i < event.results.length; ++i) {
     if (event.results[i].isFinal) {
-      document.getElementsByClassName("output")[0].value +=
-        event.results[i][0].transcript;
+      document.getElementById("output").value = event.results[i][0].transcript;
+    } else {
+      document.getElementById("interim").value = event.results[i][0].transcript;
     }
   }
 };
 
-$(document).ready(function() {
-  document
-    .getElementsByClassName("mic")[0]
-    .addEventListener("click", function() {
-      toggleStartStop();
-    });
-});
-
-function reset() {
-  recognizing = false;
-  console.log("click to speak");
-  //document.getElementsByClassName("output")[0].innerHTML = "Click to Speak";
+function showMessage(msg) {
+  document.getElementById("status").textContent = msg;
 }
+
+function stop() {
+  recognizing = false;
+}
+
+function start() {
+  recognizing = true;
+}
+
+$(document).ready(function() {
+  document.getElementById("mic").addEventListener("click", function() {
+    toggleStartStop();
+  });
+});
 
 function toggleStartStop() {
   if (recognizing) {
     recognition.stop();
-    reset();
+    showMessage("Click to speak");
+    stop();
   } else {
     recognition.start();
-    recognizing = true;
-    console.log("click to stop");
-    //document.getElementsByClassName("output")[0].innerHTML = "Click to Stop";
+    showMessage("Click to stop");
+    start();
   }
 }
