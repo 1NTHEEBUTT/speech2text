158ca2997462e0f30aec9b9513fad0ef81e512a7
tand826 <takumi.ando826@gmail.com>
2020-03-21 21:15:25 +0900

    close permission tab after get permission

--- getPermission.js
+++ getPermission.js
@@ -3,7 +3,14 @@ const getPermission = () => {
     .getUserMedia({
       audio: true
     })
-    .then(console.log("test"));
+    .then(() => {
+      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        var currTab = tabs[0];
+        if (currTab) {
+          chrome.tabs.remove(currTab);
+        }
+      });
+    });
 };
 
 getPermission();
